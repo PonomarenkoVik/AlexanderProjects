c6e7697f9784dcad90f7e852c04a81a5183028d8
v.ponomarenko <v.ponomarenko@traderevolution.com>
2020-03-15 17:53:05 +0200

    First square

---
+++ FirstProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        const int _size = 15;
+        const string symbol = "#";
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine(string.Empty);
+            }
+            Console.ReadLine();
+        }
+    }
+}
