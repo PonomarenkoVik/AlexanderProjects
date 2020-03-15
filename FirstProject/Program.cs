0e8de6bc91d0933147c1528230f0bd9dd1e09157
v.ponomarenko <v.ponomarenko@traderevolution.com>
2020-03-15 18:40:08 +0200

    add menu and square

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -6,12 +6,81 @@ using System.Threading.Tasks;
 
 namespace FirstProject
 {
+    public enum Figure { Square, Diamond, Circle, Ellipse };
+
     class Program
     {
+
         const int _size = 15;
         const string symbol = "#";
 
+
         static void Main(string[] args)
+        {
+            int menuCount = Enum.GetNames(typeof(Figure)).Length;
+            ProcessMenu(menuCount);
+        }
+
+        private static void ProcessMenu(int menuCount)
+        {
+            int chosen = 0;
+            bool needRedrawMenu = true;
+            while (true)
+            {
+                if (needRedrawMenu)
+                {
+                    Console.Clear();
+                    DrawMenu(chosen);
+                    needRedrawMenu = false;
+                }
+
+                var key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (chosen <= 0)
+                            break;
+                        chosen--;
+                        needRedrawMenu = true;
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        if (chosen >= menuCount - 1)
+                            break;
+                        chosen++;
+                        needRedrawMenu = true;
+                        break;
+
+                    case ConsoleKey.Enter:
+                        Console.Clear();
+                        DrawFigure(chosen);
+                        needRedrawMenu = true;
+                        break;
+
+                    case ConsoleKey.Escape:
+                        return;
+                }
+            }
+        }
+
+        private static void DrawFigure(int figureNumber)
+        {
+            Figure figure = (Figure)figureNumber;
+            switch (figure)
+            {
+                case Figure.Square:
+                    DrawSquare();
+                    break;
+                case Figure.Diamond:
+                    break;
+                case Figure.Circle:
+                    break;
+                case Figure.Ellipse:
+                    break;
+            }
+        }
+
+        private static void DrawSquare()
         {
             for (int i = 0; i < _size; i++)
             {
@@ -22,6 +91,22 @@ namespace FirstProject
                 Console.WriteLine(string.Empty);
             }
             Console.ReadLine();
+        }
+
+        private static void DrawMenu(int chosen)
+        {
+            string[] array = Enum.GetNames(typeof(Figure));
+            for (int i = 0; i < array.Length; i++)
+            {
+                string figure = (string)array[i];
+                string chString = i == chosen ? "*" : string.Empty;
+
+                Console.Write($"{figure}");
+                Console.SetCursorPosition(8, Console.CursorTop);
+                Console.Write($"{chString}\n");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press \"ESC\" for quit");
         }
     }
 }
